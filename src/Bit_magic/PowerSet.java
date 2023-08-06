b49d6ae259529f33058b9f8746ece77b164cbb62
Vaibhav91one <vtking03@gmail.com>
2023-08-06 15:38:13 +0530

    Kadane is Awesome

--- src/Bit_magic/PowerSet.java
+++ src/Bit_magic/PowerSet.java
@@ -1,15 +1,29 @@
 package Bit_magic;
+
 import java.util.List;
+import java.util.ArrayList;
+
 public class PowerSet {
-    public static void main(String[] args){
-        int[]nums = {1,2,3};
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        PowerSet(nums);
     }
 
-//    public static List<List<Integer>> subsets(int[] nums) {
-//
-//    }
-}
+    public static void PowerSet(int[] nums) {
+        List<List<Integer>> listOfLists
+                = new ArrayList<List<Integer>>();
 
-/*
-will work on it Tomorrow
- */
+        int n = nums.length;
+        double power = Math.pow(2, n);
+        for (int counter = 0; counter < power; counter++) {
+            List<Integer> list1 = new ArrayList<Integer>();;
+            for (int j = 0; j < n; j++) {
+                if ((counter & 1 << j) != 0) {
+                    list1.add(nums[j]);
+                }
+            }
+            listOfLists.add(list1);
+        }
+        System.out.println(listOfLists);
+    }
+}
